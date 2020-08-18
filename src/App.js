86f5fb44bc1fbbadcce1937e4162132f8a9ae961
Nick Barone <xnickbarone@gmail.com>
2020-08-18 15:12:00 -0400

    Complete page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Content = styled.div`
+	font-size: 36px;
+	margin-top: 20vh;
+	text-align: center;
+	font-family: sans-serif;
+`;
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+		<div>
+			<Content>This website is under construction</Content>
 		</div>
 	);
 }
